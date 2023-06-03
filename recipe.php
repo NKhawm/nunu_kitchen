3663d21c3c5a33fe170f454bc84721298c84c42a
Niang Khawm Huai <khawmhuai@gmail.com>
2023-06-03 11:50:40 +0100

    posts

--- recipe.php
+++ recipe.php
@@ -5,9 +5,9 @@ include 'partials/search_bar.php';
 
 
 <section class="featured">
-    <div class="container post">
+    <div class="container featured__container">
         <div class="post__thumbnail">
-            <img src="./images/Shweyinaye.png" alt="feature picture">
+            <img src="./images/Shweyinaye.png" alt="featured picture">
         </div>
         <div post__info>
             <a href="" class="category__button">Burmese</a>
@@ -29,6 +29,287 @@ include 'partials/search_bar.php';
 
     </div>
 </section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/biryani.png" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/samolinacake.jpeg" alt="samolinacake">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/noodles.jpg" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/ramen.jpeg" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/dessert.jpg" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/beefstew.png" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="post">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/icepop.png" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
+<section class="posts">
+    <div class="container posts__container">
+        <article class="post">
+            <div class="post__thumbnail">
+                <img src="images/biryani.png" alt="biryani">
+                <div class="post__info">
+                    <a href="" class="catagory__button">Asian</a>
+                    <h3 class="post__title"><a href="post.php">Fried Rice
+                        </a>
+                    </h3>
+                    <p class="post__body">
+                        Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Consequuntur nulla corporis perferendis
+                        quod quo tenetur officiis? Esse nihil molestias voluptate
+                        repellat, sint dolorem, earum possimus obcaecati aperiam
+                        dolore consequatur
+                        blanditiis.
+
+                    </p>
+                    <div class="post__author">
+                        <div class="post__author-avatar">
+                            <img src="./images/profilepic.png" alt="">
+                        </div>
+                        <div class="post__author-info">
+                            <h5>By: Niang</h5>
+                            <small>June 10,2023</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
+
 <?php
 include 'partials/share_footer.php';
 include 'partials/footer.php';
