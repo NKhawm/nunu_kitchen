1b27662227aa5b8d5deeb72d16ff9834192eb4b6
Niang Khawm Huai <khawmhuai@gmail.com>
2023-06-13 16:07:10 +0100

    dashboard

---
+++ edit-user.php
@@ -0,0 +1,40 @@
+<?php include "partials/header.php"; ?>
+
+
+<style type="text/css">
+    body {
+        background-color: #4C5959;
+    }
+</style>
+
+
+
+<section class="form__section">
+    <div class="container form__section-container">
+        <h2>Edit User</h2>
+        <div class="alert__message-error">
+            <p>This is an error message</p>
+        </div>
+        <form action="" enctype="multipart/form-data">
+            <form action="" enctype="multipart/form-data">
+                <input type="text" placeholder="First Name">
+
+                <input type="text" placeholder="Last Name">
+
+
+                <select>
+                    <option value="0">Author</option>
+                    <option value="1">Admin</option>
+                </select>
+
+                <br>
+
+
+                <button type="submit" class="sign__btn">Update User</button>
+
+            </form>
+
+    </div>
+
+</section>
+<?php include "partials/footer.php"; ?>
